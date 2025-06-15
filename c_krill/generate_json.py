63009dcced67c02b122191cb3ecbebb2f2b56ee4
ryotarofr <ryoryo.fr0608@gmail.com>
2025-06-15 12:38:55 +0900

    Refactor generate_json function to clarify library path retrieval

--- c_krill/generate_json.py
+++ c_krill/generate_json.py
@@ -2,7 +2,8 @@ import ctypes
 from . import util
 
 def generate_json(pyfile: str, root_id: str, output_path: str, env_identifier: bool):
-    _lib = util.get_liblogger_path()
+    lib_path = util.get_liblogger_path()
+    _lib = ctypes.CDLL(lib_path)
     _lib.toJson.argtypes = [
         ctypes.c_char_p, ctypes.c_size_t,  # pyfile_ptr, pyfile_len
         ctypes.c_char_p, ctypes.c_size_t,  # root_id_ptr, root_id_len
