c16ba194dcdcc67323a2e362063a22a132c85c13
ryotarofr <ryoryo.fr0608@gmail.com>
2025-06-22 23:20:53 +0900

    feat: test cases to remove unused root_id variable and simplify generate_json calls

--- test/test_generate_json.py
+++ test/test_generate_json.py
@@ -4,13 +4,12 @@ from c_krill.generate_json import generate_json
 def test_generate_json_creates_file():
 
     pyfile = "core/test/logger.py"
-    root_id = "API_ROOT_ID"
     output_path = "test/logger_output.json"
     prefix = "logger"
     target_loglevel = []
     env_identifier = True
 
-    generate_json(str(pyfile), root_id, str(output_path), prefix, target_loglevel, env_identifier)
+    generate_json(str(pyfile), str(output_path), prefix, target_loglevel, env_identifier)
 
     output_path = Path(output_path)
     assert output_path.exists(), "Output file was not created"
@@ -21,13 +20,12 @@ def test_generate_json_creates_file():
 def test_generate_json_creates_file2():
 
     pyfile = "core/test/logger.py"
-    root_id = "API_ROOT_ID"
     output_path = "test/logger_output2.json"
     prefix = "logger"
     target_loglevel = []
     env_identifier = False
 
-    generate_json(str(pyfile), root_id, str(output_path), prefix, target_loglevel, env_identifier)
+    generate_json(str(pyfile), str(output_path), prefix, target_loglevel, env_identifier)
 
     output_path = Path(output_path)
     assert output_path.exists(), "Output file was not created"
@@ -38,13 +36,12 @@ def test_generate_json_creates_file2():
 def test_generate_json_creates_file3():
 
     pyfile = "core/test/logger.py"
-    root_id = "API_ROOT_ID"
     output_path = "test/logger_output3.json"
     prefix = "logger"
     target_loglevel = ["info", "debug"]
     env_identifier = True
 
-    generate_json(str(pyfile), root_id, str(output_path), prefix, target_loglevel, env_identifier)
+    generate_json(str(pyfile), str(output_path), prefix, target_loglevel, env_identifier)
 
     output_path = Path(output_path)
     assert output_path.exists(), "Output file was not created"
@@ -53,13 +50,12 @@ def test_generate_json_creates_file3():
 def test_generate_json_creates_file4():
 
     pyfile = "core/test/logger.py"
-    root_id = "API_ROOT_ID"
     output_path = "test/logger_output4.json"
     prefix = "logger"
     target_loglevel = ["info", "debug"]
     env_identifier = False
 
-    generate_json(str(pyfile), root_id, str(output_path), prefix, target_loglevel, env_identifier)
+    generate_json(str(pyfile), str(output_path), prefix, target_loglevel, env_identifier)
 
     output_path = Path(output_path)
     assert output_path.exists(), "Output file was not created"
